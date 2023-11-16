d240d418a227fbaaabd8fe99f56e26025152cfab
Ethan Nielsen <ecnielsen11@byui.edu>
2023-11-16 16:00:45 -0700

    finished api weather activity

--- scripts/links.js
+++ scripts/links.js
@@ -1,39 +1,39 @@
-const baseURL = "https://github.com/EthanNielsen1143/wdd230";
-const linksURL = "https://raw.githubusercontent.com/EthanNielsen1143/wdd230/main/data/links.json";
+document.addEventListener('DOMContentLoaded', function(){
+    // const baseURL = "https://github.com/EthanNielsen1143/wdd230";
+    const linksURL = "https://raw.githubusercontent.com/EthanNielsen1143/wdd230/main/data/links.json";
 
 
-async function getLinks() {
-    const response = await fetch(linksURL);
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
+    async function getLinks() {
+        const response = await fetch(linksURL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        else {
+            console.log('Hello from the else statement')
+            const data = await response.json();
+            displayLinks(data);
+        }
     }
-    else {
-        console.log('Hello from the else statement')
-        const data = await response.json();
-        displayLinks(data);
+
+    const displayLinks = (data) => {
+        const lessons = data.lessons;
+
+        const linkList = document.createElement('ul');
+
+        lessons.forEach(lesson => {
+            const listItem = document.createElement('li');
+            const linkElement = document.createElement('a'); 
+
+            linkElement.href = lesson.links.url;
+            linkElement.textContent = "Week " + lesson.links.week + " | " + lesson.links.title;
+            linkElement.target = "_blank";
+
+            listItem.appendChild(linkElement);
+            linkList.appendChild(listItem);
+        });
+
+        const cardContainer = document.querySelector('.card');
+        cardContainer.appendChild(linkList);
     }
-}
-
-const displayLinks = (data) => {
-    const lessons = data.lessons;
-
-    const linkList = document.createElement('ul');
-
-    lessons.forEach(lesson => {
-        const listItem = document.createElement('li');
-        const linkElement = document.createElement('a'); 
-
-        linkElement.href = lesson.links.url;
-        linkElement.textContent = "Week " + lesson.links.week + " | " + lesson.links.title;
-        linkElement.target = "_blank";
-
-        listItem.appendChild(linkElement);
-        linkList.appendChild(listItem);
-    });
-
-    const cardContainer = document.querySelector('.card');
-    cardContainer.appendChild(linkList);
-}
-
-
-getLinks();
+    getLinks();
+});
---
+++ scripts/weatherapi.js
@@ -0,0 +1,25 @@
+const apiURL = "http://api.weatherapi.com/v1/current.json?key=8ec5743ac3494d3c99b213540231611&q=Roseville,Ca&aqi=no";
+
+async function getWeather() {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+    displayWeather(data);
+}
+
+const displayWeather = (data) => {
+    const current = data.current
+    const temp = current.temp_f
+    const condition = current.condition.text
+    const icon = current.condition.icon
+    
+    const tempElement = document.getElementById('weather-temp');
+    const conditionElement = document.getElementById('weather-desc');
+    const iconElement = document.getElementById('weather-icon');
+
+    iconElement.src = `http:${icon}`;
+    tempElement.textContent = `Temp: ${temp}\u00B0F`;
+    conditionElement.textContent = `Condition: ${condition}`;
+}
+
+getWeather();
